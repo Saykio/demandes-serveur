27db7e21635cf4b88a44ca7892bb596052448acd
unknown <pwzf831@444150AS072.corp.log.intra.laposte.fr>
2015-06-18 11:22:23 +0200

    Ajout du test de la méthode post d'une nouvelle demande

--- test/demandes-test.js
+++ test/demandes-test.js
@@ -1,16 +1,32 @@
 var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
+var moment = require('moment');
 
 describe('Test des services RH', function () {
     it('Devrait renvoyer la liste de demandes.', function (done) {
         request('localhost:8080')
             .get('/rh/demandes')
             .end(function (err, res) {
-                if (err)
-                    throw err;
-           // console.log('resultat: %s',JSON.stringify(res));
+                if (err) throw err;
                 res.should.have.property('status', 200);
+                done();
+            });
+    });
+    it('Devrait refuser la demande de congé sur solde insuffisant.', function (done) {
+        var maintenant = moment();
+        var demande = new Object();
+        demande.motif = 'Test';
+        demande.datedebut = maintenant;
+        demande.datefin = maintenant.add(11, 'days');
+        request('localhost:8080')
+            .post('/rh/demandes')
+            .send(demande)
+            .expect(400)
+            .end(function (err, res) { // .end handles the response
+                if (err) {
+                    return done(err);
+                }
                 done();
             });
     });
